9e49f20e11560ce943d1fcc165c9e6c605b3646c
Szprytny <szprytny.d@gmail.com>
2022-05-21 23:15:00 +0200

    PL?

--- text/cleaners.py
+++ text/cleaners.py
@@ -22,24 +22,7 @@ _whitespace_re = re.compile(r'\s+')
 
 # List of (regular expression, replacement) pairs for abbreviations:
 _abbreviations = [(re.compile('\\b%s\\.' % x[0], re.IGNORECASE), x[1]) for x in [
-  ('mrs', 'misess'),
-  ('mr', 'mister'),
-  ('dr', 'doctor'),
-  ('st', 'saint'),
-  ('co', 'company'),
-  ('jr', 'junior'),
-  ('maj', 'major'),
-  ('gen', 'general'),
-  ('drs', 'doctors'),
-  ('rev', 'reverend'),
-  ('lt', 'lieutenant'),
-  ('hon', 'honorable'),
-  ('sgt', 'sergeant'),
-  ('capt', 'captain'),
-  ('esq', 'esquire'),
-  ('ltd', 'limited'),
-  ('col', 'colonel'),
-  ('ft', 'fort'),
+  ('dr', 'doktor'),
 ]]
 
 
@@ -69,6 +52,7 @@ def basic_cleaners(text):
   '''Basic pipeline that lowercases and collapses whitespace without transliteration.'''
   text = lowercase(text)
   text = collapse_whitespace(text)
+  text = expand_abbreviations(text)
   return text
 
 
@@ -85,6 +69,5 @@ def english_cleaners(text):
   text = convert_to_ascii(text)
   text = lowercase(text)
   text = expand_numbers(text)
-  text = expand_abbreviations(text)
   text = collapse_whitespace(text)
   return text
